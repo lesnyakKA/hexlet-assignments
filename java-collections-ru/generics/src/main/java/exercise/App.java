dd58ddb70b700c670754767f649a8fc9f61de386
@hexlet/cli <support@hexlet.io>
2023-01-17 08:02:16 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -2,9 +2,6 @@ package exercise;
 
 import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.Map.Entry;
-
 class App {
 
     public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
